4370b3f5b9d59f37e7581cf804629b9eb83f07b6
eyajfja <70711695+eyajfja@users.noreply.github.com>
2023-05-17 16:33:31 +0800

    Update version.go

--- main/version.go
+++ main/version.go
@@ -10,7 +10,7 @@ import (
 var cmdVersion = &base.Command{
 	UsageLine: "{{.Exec}} version",
 	Short:     "Show current version of Xray",
-	Long: `Version prints the build information for Xray executables.
+	Long: `Version prints the build information for qury executables.
 	`,
 	Run: executeVersion,
 }
